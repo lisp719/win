02385f4cc155ea463805a31a6eecb5c7be57b25f
lisp719 <368034+lisp719@users.noreply.github.com>
2021-09-03 19:26:06 +0900

    feat: Add functions to profile

--- settings/profile.ps1
+++ settings/profile.ps1
@@ -1,1 +1,21 @@
-$env:Path += ";~\bin;"
+function phi {
+  Write-Output "https://via.placeholder.com/${Args}"
+}
+
+function en {
+  Start-Process "https://www.deepl.com/translator#ja/en/${Args}"
+}
+
+function twgroup {
+  $json = Get-Content "~/.tools.json" | ConvertFrom-Json
+
+  foreach ($group in $json.data.groups) {
+    $accountText = ($group.accounts | ForEach-Object { "from:{0}" -f $_.name }) -join " OR "
+
+    Start-Process "https://twitter.com/search?src=spelling_expansion_revert_click&f=live&q=exclude:replies ${accountText}"
+  }
+}
+
+function twsearch {
+  Start-Process "https://twitter.com/search?src=spelling_expansion_revert_click&f=live&q=@inbox OR exclude:replies lang:ja ${Args}"
+}
