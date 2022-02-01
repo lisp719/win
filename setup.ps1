b3efd126f69385f4c8a94cd395c557baf576bb8d
lisp719 <368034+lisp719@users.noreply.github.com>
2022-02-01 21:47:02 +0900

    feat: Add setup of scoop

--- setup.ps1
+++ setup.ps1
@@ -1,6 +1,16 @@
 # winget
 winget import --accept-package-agreements ./packages.json
 
+# scoop
+Set-ExecutionPolicy RemoteSigned -scope CurrentUser
+
+if (!(Get-Command "scoop" -ErrorAction SilentlyContinue)) {
+  Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
+  scoop bucket add extras
+}
+
+scoop install gh gibo lazydocker
+
 # etc
 if (!(Test-Path ~/Documents/PowerShell)) {
   mkdir ~/Documents/PowerShell
