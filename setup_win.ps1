613d8dfaf2abacf5bd23913216e7242ea8de6cfd
lisp719 <368034+lisp719@users.noreply.github.com>
2022-08-16 00:49:39 +0900

    feat: Stop to copy gh config file in windows

--- setup_win.ps1
+++ setup_win.ps1
@@ -7,5 +7,4 @@ Set-ExecutionPolicy RemoteSigned -scope CurrentUser
 # etc
 Copy-Item ./settings/.gitconfig ~/.gitconfig
 Copy-Item ./settings/.wslconfig ~/.wslconfig
-Copy-Item ./settings/gh/config.yml "${env:APPDATA}/Github CLI/config.yml"
 Copy-Item ./settings/terminal/settings.json ~/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json
