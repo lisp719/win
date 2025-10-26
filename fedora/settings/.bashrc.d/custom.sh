4a0f04e8756b64c919388a12f7e113af01df5054
lisp719 <368034+lisp719@users.noreply.github.com>
2025-10-26 18:03:47 +0900

    feat: Add alias to run CLI with pkgx Fixes #82

--- fedora/settings/.bashrc.d/custom.sh
+++ fedora/settings/.bashrc.d/custom.sh
@@ -4,18 +4,6 @@ export FZF_DEFAULT_COMMAND="fd -t f"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_ALT_C_COMMAND="fd -t d"
 
-# docker alias
-alias dcy="docker run --rm ghcr.io/mogurastore/dcy"
-alias gibo="docker run --rm simonwhitaker/gibo"
-
-# alias
-alias fig=docker-compose
-alias lzd="lazydocker"
-alias lzg="lazygit"
-alias mine="sudo chown -R $USER:$USER"
-
-alias yazi="pkgx yazi"
-
 # gh
 eval "$(gh completion -s bash)"
 
@@ -33,3 +21,22 @@ ZELLIJ_AUTO_EXIT=true
 
 # zoxide
 eval "$(zoxide init bash)"
+
+# pkgx aliases
+alias bat="pkgx bat"
+alias dua="pkgx dua"
+alias lazydocker="pkgx lazydocker"
+alias lazygit="pkgx lazygit"
+alias rg="pkgx rg"
+alias yazi="pkgx yazi"
+alias zellij="pkgx zellij"
+
+# docker alias
+alias dcy="docker run --rm ghcr.io/mogurastore/dcy"
+alias gibo="docker run --rm simonwhitaker/gibo"
+
+# alias
+alias fig=docker-compose
+alias lzd="lazydocker"
+alias lzg="lazygit"
+alias mine="sudo chown -R $USER:$USER"
