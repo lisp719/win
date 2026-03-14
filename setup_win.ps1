945f55ae72f79c314d428a7f567475ad842ff159
lisp719 <368034+lisp719@users.noreply.github.com>
2026-03-14 19:27:54 +0900

    feat: Sharing some configuration files between Windows and WSL

--- setup_win.ps1
+++ setup_win.ps1
@@ -5,11 +5,12 @@ Set-ExecutionPolicy RemoteSigned -scope CurrentUser
 Install-Module posh-git -Scope CurrentUser -Force
 
 # etc
-Copy-Item ./settings/.gitconfig ~/.gitconfig
-Copy-Item ./settings/.wslconfig ~/.wslconfig
 Copy-Item ./settings/jj/config.toml ~/AppData/Roaming/jj/config.toml
-Copy-Item ./settings/lazygit/config.yml ~/AppData/Local/lazygit/config.yml
-Copy-Item ./settings/Microsoft.PowerShell_profile.ps1 $PROFILE
 Copy-Item ./settings/starship.toml ~/.config/starship.toml
-Copy-Item ./settings/terminal/settings.json ~/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json
+
+Copy-Item ./settings/windows/.gitconfig ~/.gitconfig
+Copy-Item ./settings/windows/.wslconfig ~/.wslconfig
 Copy-Item ./settings/windows/.wezterm.lua ~/.wezterm.lua
+Copy-Item ./settings/windows/Microsoft.PowerShell_profile.ps1 $PROFILE
+Copy-Item ./settings/windows/lazygit/config.yml ~/AppData/Local/lazygit/config.yml
+Copy-Item ./settings/windows/terminal/settings.json ~/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json
